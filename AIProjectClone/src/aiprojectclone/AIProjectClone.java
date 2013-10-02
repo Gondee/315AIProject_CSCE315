8e816a7ba8a25b4f1a4472bfecb6ed6f2d7189cc
Gondee <joshkruger@nat-10-200-52-9.tamulink.tamu.edu>
2013-10-02 18:35:24 -0500

    Testing NetBeans Git support

---
+++ AIProjectClone/src/aiprojectclone/AIProjectClone.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aiprojectclone;
+
+/**
+ *
+ * @author joshkruger
+ */
+public class AIProjectClone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //test_Update
+        
+        // TODO code application logic here
+    }
+}
