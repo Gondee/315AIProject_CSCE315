7a91f7de10938bfcd11317ca773b5c165ad69d44
Gondee <joshkruger@nat-10-200-52-9.tamulink.tamu.edu>
2013-10-08 16:10:00 -0500

    Testing NetBeans Git support

--- AIProjectClone/src/aiprojectclone/AIProjectClone.java
+++ AIProjectClone/src/aiprojectclone/AIProjectClone.java
@@ -15,7 +15,18 @@ public class AIProjectClone {
      */
     public static void main(String[] args) {
         
-        //test_Update
+        String ExampleIP = "192.168.1.90";
+        int ExamplePort = 22;
+        int ExampleAIFlag = 1;
+        int ExampleS_CFlag = 1; 
+        
+        
+        Reverse newGame = new Reverse(ExampleIP,ExamplePort,ExampleAIFlag,ExampleS_CFlag); //Constructor that adds the values
+        newGame.controller();
+        
+       
+        
+        
         
         
         // TODO code application logic here
