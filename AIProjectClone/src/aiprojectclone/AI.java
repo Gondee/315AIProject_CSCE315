6e6e294957f17d030a3534999b3c55d17b8ba64b
Gondee <joshkruger@nat-10-200-52-9.tamulink.tamu.edu>
2013-10-08 16:49:29 -0500

    Structure

---
+++ AIProjectClone/src/aiprojectclone/AI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aiprojectclone;
+
+/**
+ *
+ * @author joshkruger
+ */
+public class AI extends GameBoard{
+    
+    
+    
+    
+    
+}
