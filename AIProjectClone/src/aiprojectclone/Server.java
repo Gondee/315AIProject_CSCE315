782cad48ce01fb69b79862516954c5185fc5b3b9
Gondee <joshkruger@nat-10-200-52-9.tamulink.tamu.edu>
2013-10-08 16:49:25 -0500

    Structure

---
+++ AIProjectClone/src/aiprojectclone/Server.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aiprojectclone;
+
+/**
+ *
+ * @author joshkruger
+ */
+public class Server extends Reverse {
+    
+    
+    
+    
+    
+    
+    
+}
