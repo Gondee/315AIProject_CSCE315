f93390b25dc2c184cbc2a813b9c6adeda393b1ec
Gondee <joshkruger@nat-10-200-52-9.tamulink.tamu.edu>
2013-10-26 00:05:17 -0500

    Client skeleton

--- AIProjectClone/src/aiprojectclone/Client.java
+++ AIProjectClone/src/aiprojectclone/Client.java
@@ -12,8 +12,18 @@ public class Client {
     
     GameBoard clientBoard; //Where ever client ends up, needs a copy of gameboard
                            //I will have the GUI updating copys of this gameboard
+    String ip;
+    String port;
+    String client;
+    String server;
+    String color;
     
-    public Client(){
+    public Client(String cli,String ser,String ipp, String ppo,String col){
+        client = cli;
+        server = ser;
+        ip = ipp;
+        port = ppo;
+        color = col;
         
     }
             
